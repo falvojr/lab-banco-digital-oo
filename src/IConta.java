4dccdbddd4e8e54cd15fd162a321e06d33e0be5a
Pedro <pedrohbprogramador@gmail.com>
2024-01-16 10:19:28 -0300

    new banco

--- src/IConta.java
+++ src/IConta.java
@@ -1,11 +1,6 @@
-
 public interface IConta {
-	
-	void sacar(double valor);
-	
-	void depositar(double valor);
-	
-	void transferir(double valor, IConta contaDestino);
-	
-	void imprimirExtrato();
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, IConta contaDestino);
+    void imprimirExtrato();
 }
