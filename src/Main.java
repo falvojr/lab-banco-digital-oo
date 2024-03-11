465407e548cc5a981a4ed969d977207c1e9b2f4b
viritoS2 <vitoramorimof@gmail.com>
2024-03-11 09:44:10 -0300

    add new project

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente vitor = new Cliente();
+		vitor.setNome("Vitor");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(vitor);
+		Conta poupanca = new ContaPoupanca(vitor);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
